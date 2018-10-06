24a2df5293803fab23a921a1697240b0157942e6
Phewinfotech <phewinfotech@gmail.com>
2018-10-06 22:08:48 +0530

    up

--- scripts/form-u12115.php
+++ scripts/form-u12115.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'vasmm89@gmail.com',
-		'to' => 'vasmm89@gmail.com'
+		'from' => 'naveen.mcts2008@gmail.com',
+		'to' => 'naveen.mcts2008@gmail.com'
 	),
 	'fields' => array(
 		'custom_U12142' => array(
